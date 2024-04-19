d957b509f7f98fc1e6f113fb65e902d7dbd88be1
AndyDCK <andy1.de-cock@epitech.eu>
2024-04-19 14:46:53 +0200

    fix stripe redirection for vercel

--- libs/helpers.ts
+++ libs/helpers.ts
@@ -1,16 +1,9 @@
 import { Price } from '../types';
 
 export const getURL = () => {
-    let url =
-        process.env.NEXT_PUBLIC_SITE_URL ??
-        process.env.NEXT_PUBLIC_VERCEL_URL ??
-        'http://localhost:3000/';
-
-    url = url.includes('http') ? url : `https://${url}`;
-    url = url.charAt(url.length - 1) === '/' ? url : `${url}/`;
-
-    return url;
+    return 'https://www.monspotify.eu/';
 };
+
 
 export const postData = async ({
     url,
